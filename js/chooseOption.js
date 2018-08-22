05a31f9e136a742f67db6c0a57f29c35bbb9f543
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-08-22 13:14:38 -0500

    Navegación parcial

---
+++ js/chooseOption.js
@@ -0,0 +1,29 @@
+window.onload = function()
+{
+    document.getElementById('start').onclick = function()
+    {
+        document.location.href = "start.html";
+    }
+
+    document.getElementById('multiplayer').onclick = function()
+    {
+        document.location.href = "multiplayer.html";
+    }
+
+    document.getElementById('help').onclick = function()
+    {
+        document.location.href = "help.html";
+    }
+
+    document.getElementById('credits').onclick = function()
+    {
+        document.location.href = "credits.html";
+    }
+}
+
+
+
+
+
+
+
