e0dd7e3ab56c9195fb5bf7dfdc3e6d7c4072bf6c
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-08-23 18:02:50 -0500

    Navegación Jugador y Multijugador.

---
+++ js/jsMulti.js
@@ -0,0 +1,14 @@
+window.onload = function()
+{
+    document.getElementById('home').onclick = function()
+    {
+        document.location.href = "menu.html";
+    }
+}
+
+
+
+
+
+
+
---
+++ js/jsTravel.js
@@ -0,0 +1,12 @@
+window.onload = function()
+{
+	document.getElementById('back').onclick = function()
+    {
+        document.location.href = "start.html";
+    }
+
+    document.getElementById('home').onclick = function()
+    {
+        document.location.href = "menu.html";
+    }
+}
