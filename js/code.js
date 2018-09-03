4083aec51470810eaec678d443a3ce9cc4284fd7
Juan Pablo Romero Londono <juan.romerol@upb.edu.co>
2018-09-03 15:52:06 -0500

    Nuevos cambios

--- js/code.js
+++ js/code.js
@@ -1,9 +1,36 @@
+var a;
+var b;
+
 window.onload = inicializar;
 
 function inicializar()
 {
+	inicializarVariables();
+    inicializarEventos();
 }
 
-function cargarJuego()
+function inicializarVariables()
 {
+    a = document.getElementById('a');
+    b = document.getElementById('b');
+    c = document.getElementById('c');
+    d = document.getElementById('d');
+    e = document.getElementById('e');
+    f = document.getElementById('f');
+    g = document.getElementById('g');
+    h = document.getElementById('h');
+    i = document.getElementById('i');
+}
+
+function inicializarEventos()
+{
+    a.addEventListener("click", imprimir);
+    b.addEventListener("click", imprimir);
+    c.addEventListener("click", imprimir);
+}
+
+function imprimir()
+{
+    console.log("a");
+    console.log(this.id);
 }
